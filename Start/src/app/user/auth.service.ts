9a6015a56bb78ab9df9ba4558d599b1b7d084031
colinah <halecolin1@gmail.com>
2020-03-07 11:09:38 -0700

    Adding aws signin

--- Start/src/app/user/auth.service.ts
+++ Start/src/app/user/auth.service.ts
@@ -7,12 +7,29 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { User } from './user.model';
 
+import { 
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser,
+  AuthenticationDetails,
+  CognitoUserSession
+ } from 'amazon-cognito-identity-js';
+
+const POOL_DATA = {
+  UserPoolId: 'us-east-2_LwUnnQ7Yk',
+  ClientId: '72np33ejfg2qdj50k709utmivs'
+}
+
+const userPool = new CognitoUserPool(POOL_DATA)
+
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
   constructor(private router: Router) {}
+  registeredUser: CognitoUser;
+
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
     const user: User = {
@@ -20,17 +37,41 @@ export class AuthService {
       email: email,
       password: password
     };
+    const attrList: CognitoUserAttribute[] = [];
     const emailAttribute = {
       Name: 'email',
       Value: user.email
     };
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+    userPool.signUp(user.username, user.password, attrList, null, (err, result) => {
+      if (err) {
+        this.authDidFail.next(true)
+        this.authIsLoading.next(false);
+        return;
+      } 
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.registeredUser = result.user
+    })
     return;
   }
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
       Username: username,
+      Pool: userPool
     };
+    const cognitoUser = new CognitoUser(userData);
+    cognitoUser.confirmRegistration(code, true, (err, result) => {
+      if(err){
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.router.navigate(['/'])
+    });
   }
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
@@ -38,6 +79,26 @@ export class AuthService {
       Username: username,
       Password: password
     };
+    const authDetails = new AuthenticationDetails(authData);
+    const userData = {
+      Username: username,
+      Pool: userPool
+    };
+    const cognitoUser = new CognitoUser(userData)
+    const that = this;
+    cognitoUser.authenticateUser(authDetails, {
+      onSuccess(result: CognitoUserSession){
+        that.authStatusChanged.next(true);
+        that.authDidFail.next(false);
+        that.authIsLoading.next(false);
+        console.log(result);
+      },
+      onFailure(err){
+        that.authDidFail.next(true);
+        that.authIsLoading.next(false);
+        console.log(err)
+      }
+    });
     this.authStatusChanged.next(true);
     return;
   }
